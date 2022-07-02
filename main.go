e2a3635c38b321da0444e5e6a066a31edb29829f
Onda Reki <rekionda0524@ezweb.ne.jp>
2022-07-02 10:52:49 +0900

    configの設定

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/reki204/go_todo/config"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+}
